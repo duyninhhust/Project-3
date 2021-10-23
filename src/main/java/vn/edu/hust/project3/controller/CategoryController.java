bd09e7fff67c4e5bc9af9021d9bf1b720b3a4d63
duyninhhust <nguyenduyninh99@gmail.com>
2021-10-23 13:27:54 +0700

    update

--- src/main/java/vn/edu/hust/project3/controller/CategoryController.java
+++ src/main/java/vn/edu/hust/project3/controller/CategoryController.java
@@ -2,10 +2,7 @@ package vn.edu.hust.project3.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vn.edu.hust.project3.model.Category;
 import vn.edu.hust.project3.service.CategoryService;
 
@@ -22,8 +19,25 @@ public class CategoryController {
         return categoryService.getAllCategory();
     }
 
+    @GetMapping("/categories/{id}")
+    public Category fetchCategoryById(@PathVariable int id){
+        return categoryService.getCategoryById(id);
+    }
+
     @PostMapping("/categories")
-    public Category saveCategory(@RequestBody Category category){
+    public Category createCategory(@RequestBody Category category){
         return categoryService.createCategory(category);
     }
+
+    @PutMapping("/categories/{id}")
+    public Category updateCategory(@PathVariable int id, @RequestBody Category category){
+        return categoryService.updateCategory(id, category);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public String deleteCategory(@PathVariable int id){
+        categoryService.deleteCategory(id);
+        return "Delete Category Completed";
+    }
+
 }
