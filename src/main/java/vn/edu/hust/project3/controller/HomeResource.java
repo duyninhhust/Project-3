49f43840ff717dfd40953edc8092da841c4db278
tuananhtranha99 <tuananhtranha99@gmail.com>
2021-11-09 21:21:57 +0700

    ta update code create, update, delete phone

--- src/main/java/vn/edu/hust/project3/controller/HomeResource.java
+++ src/main/java/vn/edu/hust/project3/controller/HomeResource.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 import vn.edu.hust.project3.model.Category;
 import vn.edu.hust.project3.model.Phone;
 import vn.edu.hust.project3.service.CategoryService;
@@ -44,11 +46,36 @@ public class HomeResource {
 
     @GetMapping("/add")
     String addPhone(Model model){
+        Phone phone = new Phone();
         List<Category> categories = categoryService.getAllCategory();
+        model.addAttribute("phone", phone);
         model.addAttribute("categories", categories);
         return "Add";
     }
 
+    @PostMapping("/savePhone")
+    RedirectView savePhone(@ModelAttribute Phone phone, Model model, RedirectAttributes redirectAttributes){
+        RedirectView redirectView;
+        String alert = "";
+        if(phone.getId() != 0){
+            Phone updatedPhone = phoneService.updatePhone(phone.getId(), phone);
+            alert = "Chỉnh sửa thông tin sản phẩm thành công";
+            redirectView = new RedirectView("/edit/" + updatedPhone.getId(), true);
+        } else {
+            Phone newPhone = phoneService.createPhone(phone);
+            alert = "Thêm mới sản phẩm thành công";
+            redirectView = new RedirectView("/edit/" + phone.getId(), true);
+        }
+        redirectAttributes.addFlashAttribute("alert", alert);
+        return redirectView;
+    }
 
 
+    @DeleteMapping("/deletePhone")
+    @ResponseBody
+    void deletePhone(@RequestParam("phoneIds[]") String[] phoneIds){
+        for (String id : phoneIds) {
+            phoneService.deletePhone(Integer.parseInt(id));
+        }
+    }
 }
--- src/main/resources/static/js/manager.js
+++ src/main/resources/static/js/manager.js
@@ -22,60 +22,32 @@ $(document).ready(function () {
     });
 
 
-    $("#addForm").submit(function(){
-        var user = {
-            name: $("#addForm input[name='name']").val(),
-            image: $("#addForm input[name='image']").val(),
-            price: $("#addForm input[name='price']").val(),
-            quantity: $("#addForm input[name='quantity']").val(),
-            introduction: $("#addForm textarea[name='introduction']").val(),
-            category: {
-                id: $("#addForm select[name='category']").val()
-            }
-        };
 
-        $.ajax({
-            type: "POST",
-            url: "products",
-            data: JSON.stringify(user),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function(msg) {
-                alert('Thêm sản phẩm thành công');
-                window.location.href = '/manage';
-            }
-        });
-
-        return false;
-    });
-
-    $("#editForm").submit(function(){
-        debugger;
-        var user = {
-            name: $("#editForm input[name='name']").val(),
-            image: $("#editForm input[name='image']").val(),
-            price: $("#editForm input[name='price']").val(),
-            quantity: $("#editForm input[name='quantity']").val(),
-            introduction: $("#editForm textarea[name='introduction']").val(),
-            category: {
-                id: $("#editForm select[name='category']").val()
-            }
-        };
-
-        $.ajax({
-            type: "POST",
-            url: "/products/" + $("#editForm input[name='id']").val(),
-            data: JSON.stringify(user),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function(msg) {
-                alert('Sửa thông tin sản phẩm thành công');
-                window.location.href = '/manage';
-            }
-        });
-
-        return false;
-    });
 });
+
+function deletePhone(){
+    var arrayCheckbox = [];
+    $("input:checkbox[name='checkbox']:checked").each(function(){
+        arrayCheckbox.push($(this).val());
+    });
+
+    if(arrayCheckbox.length > 0){
+        console.log(arrayCheckbox);
+        $.ajax({
+            url : "/deletePhone",
+            type : "delete",
+            data : {
+                phoneIds: arrayCheckbox
+            },
+            success : function a(result){
+                alert("Xóa thành công");
+                window.location.href= "/manage";
+            }
+        });
+    }
+
+}
+
+
 
 
