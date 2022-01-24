0b594fbbcc5a03482fbf4380a1da94a849c4ee9c
Ninh <63568491+duyninhhust@users.noreply.github.com>
2022-01-24 17:16:26 +0700

    Update Constant.java

--- src/main/java/vn/edu/hust/project3/config/Constant.java
+++ src/main/java/vn/edu/hust/project3/config/Constant.java
@@ -3,5 +3,5 @@ package vn.edu.hust.project3.config;
 public class Constant {
 
     public static final String EMAIL = "ninh.nguyenduy.ncc@gmail.com";
-    public static final String PASSWORD = "HoaAn2015_";
+    public static final String PASSWORD = "*****";
 }
